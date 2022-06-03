8b8b9beaf48d1af693027cd56c613581ba117f8c
Woong <rlqmatk@naver.com>
2022-06-03 14:32:40 +0900

    go__to버튼 구현완료

--- js/goToTopControl.js
+++ js/goToTopControl.js
@@ -1,51 +1,12 @@
-/* DOM 스크립트 코드 -------------------------------------------------------------- */
+(() => {
+  const $btn = document.querySelector(".up__btn");
+  console.log($btn);
 
-(function gotoTopControl(global){
-  'use strict';
-  
-  var displayTop = 100;
-  var bodyNode = null;
-  var buttonNode = null;
-  var buttonNodeDisplayValue = '';
-  var getComputedStyle = global.getComputedStyle;
-
-  function main() {
-    bodyNode = global.document.body;
-    buttonNode = document.querySelector('.buttonGoToTop');
-    buttonNodeDisplayValue = getComputedStyle(buttonNode, null).display;
-    buttonNode.addEventListener('click', handleGoToTop);
-    handleDisplayGoToTop();
-  }
-
-  function handleGoToTop() {
-    bodyNode.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  function handleDisplayGoToTop() {
-    if (global.scrollY > displayTop) {
-      buttonNode.style.display = buttonNodeDisplayValue;
+  document.addEventListener("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      $btn.classList.add("go__up");
     } else {
-      buttonNode.style.display = 'none';
+      $btn.classList.remove("go__up");
     }
-  }
-
-  global.addEventListener('DOMContentLoaded', main);
-  global.addEventListener('scroll', handleDisplayGoToTop);
-
-})(window);
-
-/* 객체 지향 JavaScript 코드 ------------------------------------------------------ */
-
-(function goToTopControl() {
-  'use strict';
-
-  function init() {
-    var appHeader = document.querySelector('.header');
-
-    new GoToTop({
-      targetTop: appHeader.getBoundingClientRect().bottom,
-    });
-  }
-
-  window.addEventListener('DOMContentLoaded', init);
-}) //();
+  });
+})();
